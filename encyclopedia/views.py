6e6ae0d7b55f53b43db461b6a7449ee2c74efb21
mihaiFilipoiu <mihai.filipoiu@gmail.com>
2024-03-20 14:39:06 +0200

    entry page FE&BE

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from . import util
+import markdown2
 
 
 def index(request):
@@ -8,3 +9,14 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry_page(request, title):
+    entry = util.get_entry(title)
+    if entry:
+        html_content = markdown2.markdown(entry)
+        return render(request, "encyclopedia/entry_page.html", {
+            "html_content": html_content,
+            "title": title
+        })
+    else:
+        return render(request, "encyclopedia/not_found.html")
+        
