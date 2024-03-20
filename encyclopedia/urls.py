eaffc20ba78c1ff0674d933e81a922ed26470b25
mihaiFilipoiu <mihai.filipoiu@gmail.com>
2024-03-20 20:10:01 +0200

    search engine

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "wiki"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.entry_page, name="entry_page")
+    path("<str:title>", views.entry_page, name="entry_page"),
+    path("search/", views.search, name="search")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from . import util
 import markdown2
@@ -19,4 +19,27 @@ def entry_page(request, title):
         })
     else:
         return render(request, "encyclopedia/not_found.html")
+
+def search(request):
+    query = request.GET.get("q")
+    print(query)
+    if query:
+        entry = util.get_entry(query)
+        if entry:
+            return redirect('wiki:entry_page', title=query)
+        
+        results = []
+        for entry in util.list_entries():
+            if query.lower() in entry.lower():
+                results.append(entry)
+        if results:
+            return render(request, "encyclopedia/search_results.html", {
+                "results": results,
+                "query": query
+            })
+        
+        return render(request, "encyclopedia/results_not_found.html", {
+            "query": query
+        })
+        
         
