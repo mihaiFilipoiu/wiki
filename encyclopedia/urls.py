bf4f91fd23b1fd91f04ac6ea849c71afec95de35
mihaiFilipoiu <mihai.filipoiu@gmail.com>
2024-03-21 18:25:20 +0200

    fixed some entry url issues

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "wiki"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:title>", views.entry_page, name="entry_page"),
+    path("wiki/<str:title>/", views.entry_page, name="entry_page"),
     path("search/", views.search, name="search"),
     path("new_page/", views.new_page, name="new_page"),
     path("edit_page/<str:title>", views.edit_page, name="edit_page"),
